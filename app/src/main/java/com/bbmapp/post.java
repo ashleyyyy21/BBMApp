acd0ed0e677525632118115bb96f4114354f576a
Ashley Wells <awells13@bulldogs.aamu.edu>
2021-04-12 22:01:36 -0500

    11

--- app/src/main/java/com/bbmapp/post.java
+++ app/src/main/java/com/bbmapp/post.java
@@ -35,6 +35,7 @@ import org.json.JSONArray;
 import java.io.File;
 import java.util.List;
 
+import fragments.ComposeFragment;
 import fragments.FavoriteFragment;
 import fragments.PostsFragment;
 
@@ -64,7 +65,7 @@ public class post extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.action_favorites:
                         Toast.makeText(post.this, "Favorites!", Toast.LENGTH_SHORT).show();
-                        fragment = new FavoriteFragment();
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_home:
                         Toast.makeText(post.this, "Home!", Toast.LENGTH_SHORT).show();
